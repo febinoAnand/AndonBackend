9f1b2cb8f2c4649fc1dc49a9b1fc24ce7d5df24d
karthik <karthik202m@gmail.com>
2024-04-25 13:12:43 +0530

    Added message id model in inbox models

--- EmailTracking/models.py
+++ EmailTracking/models.py
@@ -9,6 +9,7 @@ class Inbox(models.Model):
     to_email = models.EmailField()
     subject = models.CharField(max_length=255)
     message = models.TextField()
+    message_id = models.IntegerField(null=True)
 
     def __str__(self):
         return self.subject
