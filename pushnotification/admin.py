0de9f052416d077c370ff3fdf0f4e05f5e1553c9
karthik <karthik202m@gmail.com>
2024-05-03 20:18:44 +0530

    Added model for push notification and registered in admin

--- pushnotification/admin.py
+++ pushnotification/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class SendReportAdmin(admin.ModelAdmin):
+    list_display = ["date","time","title","message","send_to_user","users_group","delivery_status"]
+
+admin.site.register(SendReport, SendReportAdmin)
+
+class NotificationAuthAdmin(admin.ModelAdmin):
+    list_display = ("noti_token", "user_to_auth" ,)
+
+admin.site.register(NotificationAuth, NotificationAuthAdmin)
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ["application_id"]
+
+admin.site.register(Setting, SettingAdmin)
--- pushnotification/models.py
+++ pushnotification/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+
+class SendReport(models.Model):
+    date = models.DateField(null=False, blank=False)
+    time = models.TimeField(null=False, blank=False)
+    title = models.CharField(max_length=25)
+    message = models.TextField(max_length=200)
+    send_to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_reports', null=False, blank=False)
+    users_group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='sent_group', null=False, blank=False)
+    delivery_status = models.CharField(max_length=50, null=False, blank=False, default="Unknown")
+
+class NotificationAuth(models.Model):
+    user_to_auth = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, related_name='user_name' )
+    noti_token = models.TextField(max_length=50, null=False, blank=False)
+
+class Setting(models.Model):
+    application_id = models.TextField(max_length=50, null=False, blank=False)
