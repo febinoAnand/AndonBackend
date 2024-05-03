bf8eed42456b99af39da6be0cc1e8b17e80c9595
karthik <karthik202m@gmail.com>
2024-05-03 19:54:45 +0530

    Added smsgateway models and registered admin

--- smsgateway/admin.py
+++ smsgateway/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class SendReportAdmin(admin.ModelAdmin):
+    list_display = ["date","time","to_number","from_number","message","delivery_status"]
+
+admin.site.register(SendReport, SendReportAdmin)
+
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ["sid","auth_token","number"]
+
+admin.site.register(Setting, SettingAdmin)
--- smsgateway/models.py
+++ smsgateway/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
+
+class SendReport(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    to_number = models.CharField(max_length=15, null=False, blank=False)
+    from_number = models.CharField(max_length=15, null=False, blank=False)
+    message = models.TextField(max_length=100, null=False, blank=False, validators=[MaxLengthValidator(100)])
+    delivery_status = models.TextField(max_length=100, null=True, validators=[MaxLengthValidator(100)])
+
+
+class Setting(models.Model):
+    sid = models.CharField(max_length=100, default='default_sid')
+    auth_token = models.CharField(max_length=100, default='default_auth_token')
+    number = models.CharField(max_length=20)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Setting.objects.exists():
+            raise ValueError("Only one instance of Settings can be created")
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "Settings"
