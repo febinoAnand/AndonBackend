0731d7f2f5cff4fd2c4bcf32932a41277817cd15
karthik <karthik202m@gmail.com>
2024-05-04 16:15:50 +0530

    Added view url for pushnotification

--- pushnotification/serializers.py
+++ pushnotification/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from .models import *
 
-class SendReportView(serializers.ModelSerializer):
-    class meta :
+class SendReportViewSerializer(serializers.ModelSerializer):
+    class Meta :
         model = SendReport
-        fields = ("date","time","title","message","send_to_user","users_group","delivery_status")
+        fields = ('date','time','title','message','send_to_user','users_group','delivery_status')
 
-class NotificationAuthView(serializers.ModelSerializer):
-    class meta :
+class NotificationAuthViewSerializer(serializers.ModelSerializer):
+    class Meta :
         model = NotificationAuth
-        fields = ("noti_token", "user_to_auth" )
+        fields = ('noti_token', 'user_to_auth' )
 
-class SettingView(serializers.ModelSerializer):
-    class meta :
+class SettingViewSerializer(serializers.ModelSerializer):
+    class Meta :
         model = Setting
-        fields = ("application_id")
+        fields = ('__all__')
---
+++ pushnotification/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from .views import \
+    SendReportViewSet, \
+    NotificationAuthViewSet, \
+    SettingViewSet \
+
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('sendreport',SendReportViewSet)
+router.register('notificationauth',NotificationAuthViewSet)
+router.register('setting',SettingViewSet)
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
