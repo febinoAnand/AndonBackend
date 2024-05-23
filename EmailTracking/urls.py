49b97de7b5b42978ca1c7dbead89a359089e0c25
anandkumar <febinosolutions@gmail.com>
2024-05-23 19:02:23 +0530

    emailtracking app was deleted...Dont use this commit

--- EmailTracking/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('inbox', views.InboxView.as_view(), name='inbox-list'),
-    path('settings', views.SettingsView.as_view(), name='settings-detail'),
-    path('searchparameters', views.SearchParameterAPIView.as_view(), name='search-parameter'),
-]
