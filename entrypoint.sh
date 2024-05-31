4158ddc2814e5eb42253a0124f6036886d72a9c2
anandkumar <febinosolutions@gmail.com>
2024-05-31 13:53:30 +0530

    updated entrypoint in the docker section

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@
 echo "Apply Migrations"
 python manage.py migrate
 
+echo "Creating Super User"
+DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --username=admin --email=admin@example.com --noinput
+
+
 exec "$@"
