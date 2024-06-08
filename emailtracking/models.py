0b15f9940121d09e75e85ca1038ad32968c43786
RubanKumarK <ceeeerubankumar513@gmail.com>
2024-06-08 12:51:14 +0530

    Upadate group to send on Blank,Null are true

--- emailtracking/models.py
+++ emailtracking/models.py
@@ -98,7 +98,7 @@ class Trigger(models.Model):
     trigger_field = models.ForeignKey(Parameter, on_delete=models.CASCADE, blank=False, null=False,related_name="trigger_field")
     parameter_filter_list = models.ManyToManyField(ParameterFilter, blank=False, related_name="parameter_filter_lists")
     users_to_send = models.ManyToManyField(User, blank=False, related_name="trigger_user")
-    group_to_send = models.ForeignKey(Group, on_delete=models.CASCADE, blank=False, related_name="trigger_group")
+    group_to_send = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True,null=True, related_name="trigger_group")
     notification_message = models.TextField(blank=True, null=True)
     trigger_switch = models.BooleanField(default=False)
     send_sms = models.BooleanField(default=False)
@@ -139,7 +139,7 @@ class Setting(models.Model):
 
 class EmailID(models.Model):
     email = models.EmailField(unique=True)
-    setting = models.ForeignKey(Setting, on_delete=models.CASCADE, related_name='email_ids')
+    setting = models.ForeignKey(Setting,null=False, blank=False, on_delete=models.CASCADE, related_name='email_ids')
 
     def __str__(self):
         return self.email
