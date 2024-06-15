1992b8538facf44945dfe61ecbab262636044842
RubanKumarK <ceeeerubankumar513@gmail.com>
2024-06-15 12:58:08 +0530

    Issuse solved on smsgateway from_number field display smsnuber correctly

--- smsgateway/serializers.py
+++ smsgateway/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import *
 
 class SendReportViewSerializer(serializers.ModelSerializer):
+    from_number = serializers.CharField(source='from_number.smsnumber')
+
     class Meta:
         model = SendReport
-        fields = ("date","time","to_number","from_number","message","delivery_status")
+        fields = ("date", "time", "to_number", "from_number", "message", "delivery_status")
 
 class SettingViewSerializer(serializers.ModelSerializer):
     class Meta:
